51f9af548163a2f6a53df06c113d02702973ca0a
lizandro94 <leandrodfa94@gmail.com>
2019-07-05 08:04:54 -0600

    Remove overflow hidden to allow scrolling

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(function () {
     var tpModal = document.getElementsByClassName("tp-modal");
     var tpBackdrop = document.getElementsByClassName("tp-backdrop tp-active");
+    var body = document.getElementsByTagName("body")
     
     if(tpModal.length > 0){
         tpModal[0].remove();
@@ -8,4 +9,8 @@ chrome.runtime.onMessage.addListener(function () {
     if(tpBackdrop.length > 0){
         tpBackdrop[0].remove();
     }
+
+    //remove overflow hidden to allow scrolling
+    body[0].classList.remove("tp-modal-open");
+    body[0].style.overflow = "";
 });
